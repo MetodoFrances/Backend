2783dc881b6ff590c0058da607c3ad81acf35722
AngelB) <gabrielo.09.angel@gmail.com>
2022-11-03 01:17:07 -0500

    Estructura

---
+++ src/main/java/com/leasing/backend/shared/mapping/MappingConfiguration.java
@@ -0,0 +1,15 @@
+package com.leasing.backend.shared.mapping;
+
+import com.leasing.backend.shared.mapping.EnhancedModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("enhancedModelMapperConfiguration")
+public class MappingConfiguration {
+
+    @Bean
+    public EnhancedModelMapper modelMapper(){
+
+        return new EnhancedModelMapper();
+    }
+}
